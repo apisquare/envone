e8bdcc3ac963b78f184c8392ddd491883e13bb66
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2020-07-21 20:55:07 +0530

    Formatted index code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const envOneConfig = require('./src/config');
 
-module.exports.config = envOneConfig.config; 
+module.exports.config = envOneConfig.config;
 module.exports.getUserEnvironmentKeys = envOneConfig.getUserEnvironmentKeys;
