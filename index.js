cb22b1f0b845262254bec2911daa4b1b737b3793
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2020-07-21 20:51:01 +0530

    Added secret indication feature

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const envOneConfig = require('./src/config');
 
-module.exports.config = envOneConfig.config;
+module.exports.config = envOneConfig.config; 
 module.exports.getUserEnvironmentKeys = envOneConfig.getUserEnvironmentKeys;
+
+// TODO: this also should return the function getUserEnvironmentKeys, Like this
+// const envOne =require('envOne').config()
+// envOne.getUserEnvironmentKeys()
