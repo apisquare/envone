0ca8b73bcd13d981e94f5854ed97b39002c1e68c
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2020-07-27 10:51:11 +0530

    Updated example project

--- example/index.js
+++ example/index.js
@@ -9,7 +9,7 @@ console.log("[Env] ENV: ", process.env.ENV);
 console.log("[Env] BASE_URL: ", process.env.BASE_URL);
 console.log("[Env] ANALYTICS_URL: ", process.env.ANALYTICS_URL);
 
-require('envone').config({ debug: true });
+const configuredEnv = require('envone').config({ debug: true });
 
 console.log("[Updated Env] BFF_URL: ", process.env.BFF_URL);
 console.log("[Updated Env] SALESFORCE_URL: ", process.env.SALESFORCE_URL);
@@ -17,6 +17,8 @@ console.log("[Updated Env] AWS_ACCESS_KEY: ", process.env.AWS_ACCESS_KEY);
 console.log("[Updated Env] DB_CONNECTION_URL: ", process.env.DB_CONNECTION_URL);
 console.log("[Updated Env] ANALYTICS_URL: ", process.env.ANALYTICS_URL);
 console.log("[Updated Env] CONTACT_US_EMAIL: ", process.env.CONTACT_US_EMAIL);
+
+console.log(`[Configured Env] `, configuredEnv);
 
 app.get('/hello', (req, res) =>
   res.send("Hello, Welcome to Express health test server"),
